49bd132bef3a8bdc58961723173783766025ca27
Anthony Liu <liu916@gmail.com>
2018-09-01 21:37:15 -0400

    add world definitions and additions

--- raytracer.cpp
+++ raytracer.cpp
@@ -6,7 +6,19 @@
 #include <boost/qvm/vec_access.hpp>
 
 using namespace boost;
+using std::unique_ptr;
+using std::shared_ptr;
+using std::make_shared;
+using std::vector;
 
+class Shape;
+
+using World = vector<unique_ptr<Shape>>;
+
+shared_ptr<World> get_world() {
+    shared_ptr<World> world = make_shared<World>(World());
+    return world;
+}
 
 int main(int argc, char **argv) {
     qvm::vec<float,3> v{0, 0, 1};
