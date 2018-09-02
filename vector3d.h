b9fce3aff96e1dbfa35e358a4b481f394475292e
yugregory <yugreg@umich.edu>
2018-09-01 21:00:42 -0400

    implement vector class for objects to be composed of

--- vector3d.h
+++ vector3d.h
@@ -10,10 +10,56 @@ class Vector3d {
 		Vector3d() : x(T(0)), y(T(0)), z(T(0)) {}
 		Vector3d(T equiv) : x(equiv), y(equiv), z(equiv) {}
 		Vector3d(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
-
 		
-		Vector3d & normalize() {
+		T length() {
+			return (x*x) + (y*y) + (z*z);
+		}
+		T Magnitude() {
+			return sqrt(length());
+		}
+		Vector3d<T> & normalize() {
+			return *this/Magnitude();                                                  
+		}
 
+		// arithmetic operators
+		Vector3d<T> operator * (const T &f) const {
+			return Vector3d<T>(x*f, y*f, z*f);
+		}
+		Vector3d<T> operator * (const Vector3d<T> &v) const {
+			return Vector3d<T>(x*v.x, y*v.y, z*v.z);
+		}
+		Vector3d<T> & operator *= (const Vector3d<T> &v) const {
+			x*=v.x; 
+			y*=v.y;
+			x*=v.z;
+			return *this;
+		}
+		Vector3d<T> operator / (const T &f) const {
+			return Vector3d<T>(x/f, y/f, z/f);
+		}
+		T dot(const Vector3d<T> &v) const {
+			return (x*v.x) + (y*v.y) + (z*v.z);
+		}
+		Vector3d<T> operator - (const Vector3d<T> &v) const {
+			return Vector3d<T>(x-v.x, y-v.y, x-v.z);
+		}
+		Vector3d<T> operator + (const Vector3d<T> &v) const {
+			return Vector3d<T>(x+v.x, y+v.y, x+v.z);
+		}
+		Vector3d<T> & operator += (const Vector3d<T> &v) const {
+			x+=v.x;
+			y+=v.y;
+			z+=v.z;
+			return *this; 
+		}
+		Vector3d<T> operator - () const {
+			return Vector3d<T>(-x, -y, -z); 
+		}
+
+		// output
+		friend std::ostream & operator << (std::ostream & os, const Vector3d<T> & v) {
+			os << "[" << v.x << ", " << v.y << "," << v.z << "]" << endl;
+			return 0s;
 		}
 }
 
