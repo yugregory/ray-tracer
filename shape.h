96d1683a5bd88d40897433f31b78debdb345b1bc
yugregory <yugreg@umich.edu>
2018-09-02 16:28:12 -0400

    implement basic shapes in tracer

--- shape.h
+++ shape.h
@@ -1,33 +1,30 @@
-#ifndef OBJECT_H
-#define OBJECT_H
+#ifndef SHAPE_H
+#define SHAPE_H
 
 #include "vector3d.h"
 
 using Vector3df = Vector3d<float>;
 
-class Object {
+class Shape {
 	private:
-		Vector3df center;
 		Vector3df surfaceColor;
 		Vector3df emissionColor;
 		float transparency;
 		float reflection;
 	public:
-		Object(
-				const Vector3df &_center,
+		Shape(
 				const Vector3df &_surfaceColor,
 				const Vector3df &_emissionColor,
 				const float &_transparency,
 				const float &_reflection
-			) : center(_center), surfaceColor(_surfaceColor),
-				emissionColor(_emissionColor), transparency(_transparency),
-				reflection(_reflection) 
+			) : surfaceColor(_surfaceColor), emissionColor(_emissionColor), 
+				transparency(_transparency), reflection(_reflection) 
 		{}
 		virtual bool intersect(
 				const Vector3df &rayOrig,
 				const Vector3df &rayDir,
 				float &t0,
-				float &t1) const {}
+				float &t1) const {};
 };
 
 #endif
--- sphere.h
+++ sphere.h
@@ -1,9 +1,38 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
-class Sphere {
+#include "shape.h" 
+#include "Vector3d.h"
+
+using Vector3df = Vector3d<float>;
+
+class Sphere : public Shape {
+	private:
+		Vector3df center;
+		float radius;
+		float radiusSquared;
 	public:
+		Sphere(
+				const Vector3df &_center,
+				const float &_radius,
+				const float &_radiusSquared;
+				const Vector3df &_surfaceColor,
+				const Vector3df &_emissionColor,
+				const float &_transparency,
+				const float &_reflection
+			) : Shape(_surfaceColor, _emissionColor, 
+				_transparency, _reflection), 
+				Center(_center), radius(_radius),
+				radiusSquared(_radiusSquared) 
+		{}
 		
-}
+		virtual bool intersect(
+				const Vector3df &rayOrig, 
+				const Vector3df &rayDir,
+				float &t0,
+				float &t1) const override {
+
+		}
+};
 
 #endif
