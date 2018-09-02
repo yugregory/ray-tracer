a235ccc1fa374e2b0528b874935235a80ad42a4c
Anthony Liu <liu916@gmail.com>
2018-09-01 22:03:05 -0400

    add signatures

--- raytracer.cpp
+++ raytracer.cpp
@@ -11,13 +11,33 @@ using std::shared_ptr;
 using std::make_shared;
 using std::vector;
 
-class Shape;
-
+using Shape = float;//TODO change
 using World = vector<unique_ptr<Shape>>;
+using Color = qvm::vec<float, 3>;
+using Vec = qvm::vec<float, 3>;
+
+class Config {
+public:
+    unsigned width;
+    unsigned height;
+    float fov;
+};
 
 shared_ptr<World> get_world() {
     shared_ptr<World> world = make_shared<World>(World());
     return world;
+}
+
+Color trace(
+    const Vec &rayorig,
+    const Vec &raydir,
+    shared_ptr<World> world,
+    const int &depth
+) {
+    return {0, 0, 0};
+}
+
+void render(shared_ptr<World> world, const Config config) {
 }
 
 int main(int argc, char **argv) {
