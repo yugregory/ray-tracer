1a8979188a9758345250c5d21ef85110b10e355c
Anthony Liu <liu916@gmail.com>
2018-09-11 16:53:21 -0400

    NO TABS

--- shape.h
+++ shape.h
@@ -8,28 +8,28 @@ using namespace boost;
 using Vec = qvm::vec<float, 3>;
 
 class Shape {
-	protected:
-		Vec surfaceColor;
-		Vec emissionColor;
-		float transparency;
-		float reflection;
-	public:
-		Shape(
-				const Vec &_surfaceColor,
-				const Vec &_emissionColor,
-				const float &_transparency,
-				const float &_reflection
-			) : surfaceColor(_surfaceColor), emissionColor(_emissionColor), 
-				transparency(_transparency), reflection(_reflection) 
-		{}
+    protected:
+        Vec surfaceColor;
+        Vec emissionColor;
+        float transparency;
+        float reflection;
+    public:
+        Shape(
+                const Vec &_surfaceColor,
+                const Vec &_emissionColor,
+                const float &_transparency,
+                const float &_reflection
+            ) : surfaceColor(_surfaceColor), emissionColor(_emissionColor), 
+                transparency(_transparency), reflection(_reflection) 
+        {}
 
-		virtual bool intersect(
-				const Vec &rayOrig,
-				const Vec &rayDir,
-				float &t0,
-				float &t1) const = 0;
+        virtual bool intersect(
+                const Vec &rayOrig,
+                const Vec &rayDir,
+                float &t0,
+                float &t1) const = 0;
 
-		virtual ~Shape() = default;
+        virtual ~Shape() = default;
 };
 
 #endif
